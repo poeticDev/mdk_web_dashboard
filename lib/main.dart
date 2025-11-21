7788c91c86a8567434568f885e117e2cf07b377e
poeticdev23 <poeticdev23@gmail.com>
2025-11-21 13:42:10 +0900

    Feat(theme): main.dart 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:web_dashboard/core/auth/application/auth_controller.dart';
 import 'package:web_dashboard/di/service_locator.dart';
 import 'package:web_dashboard/routes/app_router.dart';
+import 'package:web_dashboard/theme/app_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final AdaptiveThemeMode? savedThemeMode = await AdaptiveTheme.getThemeMode();
   await initDependencies();
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(ProviderScope(child: MyApp(initialThemeMode: savedThemeMode)));
 }
 
 class MyApp extends ConsumerStatefulWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, this.initialThemeMode});
+
+  final AdaptiveThemeMode? initialThemeMode;
 
   @override
   ConsumerState<MyApp> createState() => _MyAppState();
@@ -30,15 +35,19 @@ class _MyAppState extends ConsumerState<MyApp> {
   @override
   Widget build(BuildContext context) {
     final GoRouter router = ref.watch(appRouterProvider);
-    return MaterialApp.router(
-      title: 'MDK Web Dashboard',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        fontFamily: 'Pretendard Variable',
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff334479)),
-      ),
-      routerConfig: router,
+    return AdaptiveTheme(
+      light: AppTheme.light(),
+      dark: AppTheme.dark(),
+      initial: widget.initialThemeMode ?? AdaptiveThemeMode.system,
+      builder: (ThemeData lightTheme, ThemeData darkTheme) {
+        return MaterialApp.router(
+          title: 'MDK Web Dashboard',
+          debugShowCheckedModeBanner: false,
+          theme: lightTheme,
+          darkTheme: darkTheme,
+          routerConfig: router,
+        );
+      },
     );
   }
 }
